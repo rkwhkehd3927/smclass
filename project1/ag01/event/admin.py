fbcb42f99567a6791e1ce4dfdcc4d3dc21868ab3
rkwhkehd3927 <rkwhkehd3927@naver.com>
2024-12-06 18:03:46 +0900

    1206(출첵,응모권,이벤트주소공유)

--- project1/ag01/event/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from event.models import Attendance
-
-@admin.register(Attendance)
-class AttendanceAdmin(admin.ModelAdmin):
-  list_display = ['aId','aDate','count','aNumber']
--- project1/ag01/event/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-app_name = 'event'
-urlpatterns = [
-    path('calendar/', views.calendar, name='calendar'),
-    path('apply/', views.apply, name='apply'),
-]
-
